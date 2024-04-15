ca49738f0aa613ef4d3b3343384927fc005fbfbe
ritesh <ritesh_2239176@saitm.ac.in>
2024-04-15 11:56:30 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,31 @@
+// Object types
+
+// let object = {
+//     Name:"Ritesh"
+//     Age: 19,
+//     Number: 7011341628
+// };
+// console.log(object);
+
+// let ab = 1;
+// ab++;
+// a+b 
+// "a"
+
+// console.log(" b ="+ b);
+// let sum = a+b;
+// let diff = a-b;
+// let product = a*b;
+// let quo = a / b;
+// let re = a % b;
+
+// console.log("a + b =",sum);
+// console.log("a - b =",sum);
+// console.log("a * b =",sum);
+// console.log("a / b =",sum);
+// console.log("a % b =",re);
+
+// a = 10
+// b = 3
+
+console.log()
